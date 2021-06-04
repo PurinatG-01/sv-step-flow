113dc4fd13130e0d55b720050eccd0b07b829b7c
Purinat <fame3js49@gmail.com>
2021-06-04 15:37:17 +0700

    feat: change step child component

--- src/components/Steps/StepsProvider.svelte
+++ src/components/Steps/StepsProvider.svelte
@@ -1,63 +1,62 @@
 <script>
-	import { fade, fly } from 'svelte/transition';
-	import TodoList from '@/components/Todos/TodoList.svelte'
-	import { generateFakeUserDatas } from '@/util/generateFakeUserDatas.js'
-	import Step from '@/components/Steps/Step.svelte'
+  import { fade, fly } from "svelte/transition";
+  import TodoList from "@/components/Todos/TodoList.svelte";
+  import { generateFakeUserDatas } from "@/util/generateFakeUserDatas.js";
+  import Step from "@/components/Steps/Step.svelte";
 
-	let stepChanger = 0
-	let steps = [1,2,3,4,5]
-	let action = false
-	
-	let animation = {
-		lr: { x: -200, duration: 500 },
-		rl: { x: 200, duration: 500 },
-	}
-	
-	const nextStep = ()=>{
-		action = true
-		if(stepChanger < steps.length-1 ){
-			stepChanger++
-		}else{
-			stepChanger = 0
-		}
-	}
-	const prevStep = ()=>{
-		action = false
-		if(stepChanger > 0){
-			stepChanger--
-		}else{
-			stepChanger = steps.length-1
-		}
-	}
-	
-	
+  let stepChanger = 1;
+  let stepsLimit = 3;
+  let action = false;
+
+  let animation = {
+    lr: { x: -200, duration: 500 },
+    rl: { x: 200, duration: 500 },
+  };
+
+  const nextStep = () => {
+    action = true;
+    if (stepChanger < stepsLimit) {
+      stepChanger++;
+    } else {
+      stepChanger = 1;
+    }
+  };
+  const prevStep = () => {
+    action = false;
+    if (stepChanger > 1) {
+      stepChanger--;
+    } else {
+      stepChanger = stepsLimit;
+    }
+  };
 </script>
 
 <div>
-	<h1>
-		<slot name="header"></slot>
-	</h1>
-	<button on:click={prevStep} >prev</button>
-	<button on:click={nextStep} >next</button>
-	{stepChanger}
+  <button on:click={prevStep}>prev</button>
+  <button on:click={nextStep}>next</button>
+  {stepChanger}
 </div>
-
-{#each steps as step}
-	{#if stepChanger == step-1}
-		<div transition:fade class="step-wrapper">
-			<!-- <TodoList data={generateFakeUserDatas(20)}/> -->
-			<Step number={step} />
-		</div>
-	{/if}
-{/each}
-
-<div>
-	<slot name="footer"></slot>
+<div class="step-container">
+  {#if stepChanger == 1}
+    <div out:fade class="step-wrapper" in:fly={{x:-200, opacity: 0}}>
+      <TodoList data={generateFakeUserDatas(10)}/>
+    </div>
+  {:else if stepChanger == 2}
+    <div out:fade class="step-wrapper" in:fly={{x:-200, opacity: 0}}>
+      <TodoList data={generateFakeUserDatas(5)}/>
+    </div>
+  {:else if stepChanger == 3}
+    <div out:fade class="step-wrapper" in:fly={{x:-200, opacity: 0}}>
+      <Step number={3} />
+    </div>
+  {/if}
 </div>
-
 <style>
-	.step-wrapper{
-		position: relative;
-
-	}
+  .step-wrapper {
+    position: absolute;
+    z-index: 1;
+  }
+  .step-container{
+	  /* background:#f0f0f0; */
+  }
 </style>
